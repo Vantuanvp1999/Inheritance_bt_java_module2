149ffd032fcc437e0026f4c4a4b7c69942bc3c90
Tuân <nguyenvantuanvp1999@gmail.com>
2025-04-11 10:39:30 +0700

    addbt2

---
+++ point 2d 3d/src/Main.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Point3D p =new Point3D(2,1,4);
+        System.out.println(p);
+    }
+}
