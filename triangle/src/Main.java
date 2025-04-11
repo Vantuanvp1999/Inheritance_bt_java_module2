ae49251a2662414593b6afcc9174bb3d4b62fbc9
Tuân <nguyenvantuanvp1999@gmail.com>
2025-04-11 11:18:20 +0700

    addbt4

---
+++ triangle/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle("red",true,3,4,5);
+        System.out.println(triangle);
+    }
+}
